3fa85a508e9b757240695b38f48499862dcb3ee4
yadukrishnakp <yadukrishnakp007@gmail.com>
2022-04-23 16:28:09 +0530

    A new API created for checking number of likes and dislikes

--- LikeandDislike/urls.py
+++ LikeandDislike/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = {
     path('show_all_stories/', views.ShowAllStories, name="show_all_stories"),
     path('show_story/<str:pk>', views.ShowStory, name="show_story"),
+    path('like_and_dislike/<str:pk>', views.CountLikeDislike, name="like_and_dislike")
 }
--- LikeandDislike/views.py
+++ LikeandDislike/views.py
@@ -21,3 +21,14 @@ def ShowStory(request, pk):
     story = Story.objects.get(id=pk)
     serializer = StorySerializer(story)
     return Response(serializer.data)
+
+
+# Check likes and dislikes of individual stories
+@api_view(['GET'])
+def CountLikeDislike(request, pk):
+    story = Story.objects.get(id=pk)
+    data = story.storyinformation_set.all()
+    like_count = data.filter(story_status='like').count()
+    dislike_count = data.filter(story_status='dislike').count()
+    contexts = {'likes': like_count, 'dislikes': dislike_count}
+    return Response(contexts)
